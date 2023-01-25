84ec6e65d8dbc04f1ad10c4cd0c3ea98bfd46ae2
Djurica Surla <djurica.surla@imaginecurve.com>
2023-01-25 17:30:54 +0100

    Finish writing service tests

--- internal/service/questionService_test.go
+++ internal/service/questionService_test.go
@@ -264,3 +264,321 @@ func TestService_GetQuestionByID(t *testing.T) {
 		assert.Error(t, err)
 	})
 }
+
+func TestService_CreateQuestion(t *testing.T) {
+	t.Run("Should create question successfuly", func(t *testing.T) {
+		ctx := context.Background()
+		mocks, svc := initMockService(t)
+
+		questionCreationDTO := service.QuestionCreationDTO{
+			Body: "first-question",
+			Options: []service.QuestionOptionCreationDTO{
+				{
+					Body:    "first-option",
+					Correct: false,
+				},
+				{
+					Body:    "second-option",
+					Correct: false,
+				},
+			},
+		}
+
+		optionCreationDTO1 := service.QuestionOptionCreationDTO{
+			Body:    "first-option",
+			Correct: false,
+		}
+		optionCreationDTO2 := service.QuestionOptionCreationDTO{
+			Body:    "second-option",
+			Correct: false,
+		}
+
+		storedQuestion := entity.Question{
+			ID:   1,
+			Body: "first-question",
+		}
+
+		storedQuestionOption := []entity.QuestionOption{
+			{ID: 1,
+				Body:    "first-option",
+				Correct: false,
+			},
+			{
+				ID:      2,
+				Body:    "second-option",
+				Correct: false,
+			},
+		}
+
+		expectedResult := service.QuestionDTO{
+			ID:   1,
+			Body: "first-question",
+			Options: []service.QuestionOptionDTO{
+				{
+					ID:      1,
+					Body:    "first-option",
+					Correct: false,
+				},
+				{
+					ID:      2,
+					Body:    "second-option",
+					Correct: false,
+				},
+			},
+		}
+
+		gomock.InOrder(
+			mocks.questionStorer.EXPECT().CreateQuestion(ctx, questionCreationDTO.Body).Return(1, nil),
+			mocks.questionOptionStorer.EXPECT().CreateQuestionOption(ctx, 1, optionCreationDTO1).Return(nil),
+			mocks.questionOptionStorer.EXPECT().CreateQuestionOption(ctx, 1, optionCreationDTO2).Return(nil),
+			mocks.questionStorer.EXPECT().GetQuestionByID(ctx, 1).Return(storedQuestion, nil),
+			mocks.questionOptionStorer.EXPECT().GetQuestionOptions(ctx, 1).Return(storedQuestionOption, nil),
+		)
+
+		question, err := svc.CreateQuestion(ctx, questionCreationDTO)
+		assert.EqualValues(t, expectedResult, question)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Should fail because creating question in the database fails", func(t *testing.T) {
+		ctx := context.Background()
+
+		mocks, svc := initMockService(t)
+		someErr := errors.New("some-error")
+
+		gomock.InOrder(
+			mocks.questionStorer.EXPECT().CreateQuestion(ctx, "").Return(0, someErr),
+		)
+
+		question, err := svc.CreateQuestion(ctx, service.QuestionCreationDTO{})
+		assert.Equal(t, service.QuestionDTO{}, question)
+		assert.Error(t, err)
+	})
+
+	t.Run("Should fail because creating question options in the database fails", func(t *testing.T) {
+		ctx := context.Background()
+
+		mocks, svc := initMockService(t)
+		someErr := errors.New("some-error")
+
+		questionCreationDTO := service.QuestionCreationDTO{
+			Body: "first-question",
+			Options: []service.QuestionOptionCreationDTO{
+				{
+					Body:    "first-option",
+					Correct: false,
+				},
+			},
+		}
+
+		optionCreationDTO1 := service.QuestionOptionCreationDTO{
+			Body:    "first-option",
+			Correct: false,
+		}
+
+		gomock.InOrder(
+			mocks.questionStorer.EXPECT().CreateQuestion(ctx, questionCreationDTO.Body).Return(1, nil),
+			mocks.questionOptionStorer.EXPECT().CreateQuestionOption(ctx, 1, optionCreationDTO1).Return(someErr),
+		)
+
+		question, err := svc.CreateQuestion(ctx, questionCreationDTO)
+		assert.Equal(t, service.QuestionDTO{}, question)
+		assert.Error(t, err)
+	})
+}
+
+func TestService_UpdateQuestion(t *testing.T) {
+	t.Run("Should update question successfuly", func(t *testing.T) {
+		ctx := context.Background()
+		mocks, svc := initMockService(t)
+
+		questionCreationDTO := service.QuestionCreationDTO{
+			Body: "first-question",
+			Options: []service.QuestionOptionCreationDTO{
+				{
+					Body:    "first-option",
+					Correct: false,
+				},
+				{
+					Body:    "second-option",
+					Correct: false,
+				},
+			},
+		}
+
+		optionCreationDTO1 := service.QuestionOptionCreationDTO{
+			Body:    "first-option",
+			Correct: false,
+		}
+		optionCreationDTO2 := service.QuestionOptionCreationDTO{
+			Body:    "second-option",
+			Correct: false,
+		}
+
+		storedQuestion := entity.Question{
+			ID:   1,
+			Body: "first-question",
+		}
+
+		storedQuestionOption := []entity.QuestionOption{
+			{ID: 1,
+				Body:    "first-option",
+				Correct: false,
+			},
+			{
+				ID:      2,
+				Body:    "second-option",
+				Correct: false,
+			},
+		}
+
+		expectedResult := service.QuestionDTO{
+			ID:   1,
+			Body: "first-question",
+			Options: []service.QuestionOptionDTO{
+				{
+					ID:      1,
+					Body:    "first-option",
+					Correct: false,
+				},
+				{
+					ID:      2,
+					Body:    "second-option",
+					Correct: false,
+				},
+			},
+		}
+
+		gomock.InOrder(
+			mocks.questionStorer.EXPECT().UpdateQuestion(ctx, 1, questionCreationDTO.Body).Return(1, nil),
+			mocks.questionOptionStorer.EXPECT().DeleteQuestionOptions(ctx, 1).Return(nil),
+			mocks.questionOptionStorer.EXPECT().CreateQuestionOption(ctx, 1, optionCreationDTO1).Return(nil),
+			mocks.questionOptionStorer.EXPECT().CreateQuestionOption(ctx, 1, optionCreationDTO2).Return(nil),
+			mocks.questionStorer.EXPECT().GetQuestionByID(ctx, 1).Return(storedQuestion, nil),
+			mocks.questionOptionStorer.EXPECT().GetQuestionOptions(ctx, 1).Return(storedQuestionOption, nil),
+		)
+
+		question, err := svc.UpdateQuestion(ctx, 1, questionCreationDTO)
+		assert.EqualValues(t, expectedResult, question)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Should return empty dto response because update affects no rows", func(t *testing.T) {
+		ctx := context.Background()
+
+		mocks, svc := initMockService(t)
+
+		questionCreationDTO := service.QuestionCreationDTO{
+			Body: "first-question",
+			Options: []service.QuestionOptionCreationDTO{
+				{
+					Body:    "first-option",
+					Correct: false,
+				},
+				{
+					Body:    "second-option",
+					Correct: false,
+				},
+			},
+		}
+
+		gomock.InOrder(
+			mocks.questionStorer.EXPECT().UpdateQuestion(ctx, 1, questionCreationDTO.Body).Return(0, nil),
+		)
+
+		question, err := svc.UpdateQuestion(ctx, 1, questionCreationDTO)
+		assert.Equal(t, service.QuestionDTO{}, question)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Should return error because deleting previous option fails", func(t *testing.T) {
+		ctx := context.Background()
+
+		mocks, svc := initMockService(t)
+
+		someErr := errors.New("some-error")
+
+		questionCreationDTO := service.QuestionCreationDTO{
+			Body: "first-question",
+			Options: []service.QuestionOptionCreationDTO{
+				{
+					Body:    "first-option",
+					Correct: false,
+				},
+				{
+					Body:    "second-option",
+					Correct: false,
+				},
+			},
+		}
+
+		gomock.InOrder(
+			mocks.questionStorer.EXPECT().UpdateQuestion(ctx, 1, questionCreationDTO.Body).Return(1, nil),
+			mocks.questionOptionStorer.EXPECT().DeleteQuestionOptions(ctx, 1).Return(someErr),
+		)
+
+		question, err := svc.UpdateQuestion(ctx, 1, questionCreationDTO)
+		assert.Equal(t, service.QuestionDTO{}, question)
+		assert.Error(t, err)
+	})
+
+	t.Run("Should return error because creating new options fails", func(t *testing.T) {
+		ctx := context.Background()
+
+		mocks, svc := initMockService(t)
+
+		someErr := errors.New("some-error")
+
+		questionCreationDTO := service.QuestionCreationDTO{
+			Body: "first-question",
+			Options: []service.QuestionOptionCreationDTO{
+				{
+					Body:    "first-option",
+					Correct: false,
+				},
+			},
+		}
+		optionCreationDTO1 := service.QuestionOptionCreationDTO{
+			Body:    "first-option",
+			Correct: false,
+		}
+
+		gomock.InOrder(
+			mocks.questionStorer.EXPECT().UpdateQuestion(ctx, 1, questionCreationDTO.Body).Return(1, nil),
+			mocks.questionOptionStorer.EXPECT().DeleteQuestionOptions(ctx, 1).Return(nil),
+			mocks.questionOptionStorer.EXPECT().CreateQuestionOption(ctx, 1, optionCreationDTO1).Return(someErr),
+		)
+
+		question, err := svc.UpdateQuestion(ctx, 1, questionCreationDTO)
+		assert.Equal(t, service.QuestionDTO{}, question)
+		assert.Error(t, err)
+	})
+}
+
+func TestService_DeleteQuestion(t *testing.T) {
+	t.Run("Should delete question successfuly", func(t *testing.T) {
+		ctx := context.Background()
+		mocks, svc := initMockService(t)
+
+		gomock.InOrder(
+			mocks.questionStorer.EXPECT().DeleteQuestion(ctx, 1).Return(nil),
+		)
+
+		err := svc.DeleteQuestion(ctx, 1)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Should fail to delete question when database fails", func(t *testing.T) {
+		ctx := context.Background()
+		mocks, svc := initMockService(t)
+
+		someErr := errors.New("some-error")
+
+		gomock.InOrder(
+			mocks.questionStorer.EXPECT().DeleteQuestion(ctx, 1).Return(someErr),
+		)
+
+		err := svc.DeleteQuestion(ctx, 1)
+		assert.Error(t, err)
+	})
+}
