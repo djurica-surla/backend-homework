0d0dd5545af3d09ef7e3d3cfd2925a21c2d58b63
Djurica <djurica.surla@gmail.com>
2023-01-25 02:40:38 +0100

    Update comments

--- internal/service/dto.go
+++ internal/service/dto.go
@@ -1,26 +1,26 @@
 package service
 
-// Represents options for question.
+// Question option dto used for response.
 type QuestionOptionDTO struct {
 	ID      int    `json:"id"`
 	Body    string `json:"body"`
 	Correct bool   `json:"correct"`
 }
 
-// Represents question.
+// Question dto used for response.
 type QuestionDTO struct {
 	ID      int                 `json:"id"`
 	Body    string              `json:"body"`
 	Options []QuestionOptionDTO `json:"options"`
 }
 
-// Represents question optioon entity for question creation.
+// Question option dto used for create and update request.
 type QuestionOptionCreationDTO struct {
 	Body    string `json:"body" validate:"required"`
 	Correct bool   `json:"correct"`
 }
 
-// Represents entity used in question creation.
+// Question dto used for create and update requests.
 type QuestionCreationDTO struct {
 	Body    string                      `json:"body" validate:"required"`
 	Options []QuestionOptionCreationDTO `json:"options" validate:"dive,required"`
